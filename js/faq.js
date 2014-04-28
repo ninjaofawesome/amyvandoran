271c5146feebf3325b32804b75bcfd53b149c036
Hannah Nordgren <hey@hereshannahs.info>
2014-04-28 10:14:56 -0400

    added more sections, need to refactor

--- js/faq.js
+++ js/faq.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 
   $('.q1-hide').hide();
   $('#q1up').hide();
+  $('.q2-hide').hide();
+  $('#q2up').hide();
+  $('.q3-hide').hide();
+  $('#q3up').hide();
 
   $('#q1').click(function(e){
     $('.q1-hide').slideDown().fadeIn();
@@ -14,6 +18,30 @@ $(document).ready(function(){
     $('.q1-hide').slideUp().hide();
     $('#q1up').hide();
     $('#q1').fadeIn();
+  });
+
+   $('#q2').click(function(e){
+    $('.q2-hide').slideDown().fadeIn();
+    $('#q2').hide();
+    $('#q2up').fadeIn();
+  });
+
+  $('#q2up').click(function(e){
+    $('.q2-hide').slideUp().hide();
+    $('#q2up').hide();
+    $('#q2').fadeIn();
+  });
+
+  $('#q3').click(function(e){
+    $('.q3-hide').slideDown().fadeIn();
+    $('#q3').hide();
+    $('#q3up').fadeIn();
+  });
+
+  $('#q3up').click(function(e){
+    $('.q3-hide').slideUp().hide();
+    $('#q3up').hide();
+    $('#q3').fadeIn();
   });
 
 });
