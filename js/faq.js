e147a2c10c7d264f49befba8786359f554c4afd9
Hannah Nordgren <hey@hereshannahs.info>
2014-04-28 08:59:56 -0400

    got the first question slide and hide done

--- js/faq.js
+++ js/faq.js
@@ -2,9 +2,18 @@ $(document).ready(function(){
   console.log('slider has loaded');
 
   $('.q1-hide').hide();
+  $('#q1up').hide();
 
   $('#q1').click(function(e){
-    $('.q1-hide').slidedown().fadeIn();
+    $('.q1-hide').slideDown().fadeIn();
+    $('#q1').hide();
+    $('#q1up').fadeIn();
+  });
+
+  $('#q1up').click(function(e){
+    $('.q1-hide').slideUp().hide();
+    $('#q1up').hide();
+    $('#q1').fadeIn();
   });
 
 });
