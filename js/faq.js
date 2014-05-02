eabc1eb178646901eb188580a311cc251569bfc9
Hannah Nordgren <hey@hereshannahs.info>
2014-05-02 09:07:16 -0400

    responsive modal box

--- js/faq.js
+++ js/faq.js
@@ -7,6 +7,12 @@ $(document).ready(function(){
   $('#q2up').hide();
   $('.q3-hide').hide();
   $('#q3up').hide();
+  $('.q4-hide').hide();
+  $('#q4up').hide();
+  $('.q5-hide').hide();
+  $('#q5up').hide();
+  $('.q6-hide').hide();
+  $('#q6up').hide();
 
   $('#q1').click(function(e){
     $('.q1-hide').slideDown().fadeIn();
@@ -42,6 +48,40 @@ $(document).ready(function(){
     $('.q3-hide').slideUp().hide();
     $('#q3up').hide();
     $('#q3').fadeIn();
+  });
+
+   $('#q4').click(function(e){
+    $('.q4-hide').slideDown().fadeIn();
+    $('#q4').hide();
+    $('#q4up').fadeIn();
+  });
+
+  $('#q4up').click(function(e){
+    $('.q4-hide').slideUp().hide();
+    $('#q4up').hide();
+    $('#q4').fadeIn();
+  });
+   $('#q5').click(function(e){
+    $('.q5-hide').slideDown().fadeIn();
+    $('#q5').hide();
+    $('#q5up').fadeIn();
+  });
+
+  $('#q5up').click(function(e){
+    $('.q5-hide').slideUp().hide();
+    $('#q5up').hide();
+    $('#q5').fadeIn();
+  });
+   $('#q6').click(function(e){
+    $('.q6-hide').slideDown().fadeIn();
+    $('#q6').hide();
+    $('#q6up').fadeIn();
+  });
+
+  $('#q6up').click(function(e){
+    $('.q6-hide').slideUp().hide();
+    $('#q6up').hide();
+    $('#q6').fadeIn();
   });
 
 });
