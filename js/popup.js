dc90c96da2df61dc68f0cd092624dc4084d02d57
Hannah Nordgren <hey@hereshannahs.info>
2014-05-19 14:01:20 -0400

    started working on modal

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   console.log('popup has loaded');
 
+  // function popupwindow(url, title, w, h) {
+  //   var left = (screen.width/2)-(w/2);
+  //   var top = (screen.height/2)-(h/2);
+  //   return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  // }
+
+
   $('.modalDialog').hide().fadeIn(600);
   $('.btn-primary-centered').click(function(e){
   $('.modalDialog').fadeOut(600);
