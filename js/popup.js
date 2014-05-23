030afdf3139332b498424cb95401b310b688863b
Hannah Nordgren <hey@hereshannahs.info>
2014-05-22 23:44:57 -0400

    revisiting the modal, need to check firing once and only once.

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
   console.log('popup has loaded');
+    if($.cookie('msg') == null)
+        {
+            $('#myModal').modal('show');
+            $.cookie('msg', 'str');
+        }else{
+            $("div#myModal.modal").css('display','none');
+        }
 
-  // function popupwindow(url, title, w, h) {
-  //   var left = (screen.width/2)-(w/2);
-  //   var top = (screen.height/2)-(h/2);
-  //   return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-  // }
-
-
-  $('.modalDialog').hide().fadeIn(600);
-  $('.btn-primary-centered').click(function(e){
-  $('.modalDialog').fadeOut(600);
-
-  var hideAgain = function(){
-    $('.modalDialog').addClass('hidden');
-  }
-
-     $.cookie('addCookie', 'hideAgain', {expires: 1});
-     $.cookie(('addCookie') );
-
-  });
+  // $('#myModal').modal('show');
 
 
 });
